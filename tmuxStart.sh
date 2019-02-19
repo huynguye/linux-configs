0d9c6e32eddc503cabb867874514ef190f3e60ea
Huy Nguyen <huyn@ldproducts.com>
2019-02-18 17:53:55 -0800

    Updated tmux sessions/windows/panes.

--- tmuxStart.sh
+++ tmuxStart.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
-# var for session name (to avoid repeated occurences)
-sn=$RANDOM
-
-# 1st session
+# 1st session (crana/gemini/aa)
 cd $HOME
-tmux new-session -s "code" -n cranaback -d -c "/home/crana-admin/workspace/crana/backend/www"
+tmux new-session -s "crana" -n gemini -d -c "/home/debian-admin/repositories/ld-api/api/www"
+tmux split-window -d -p 30 -h -c "/home/debian-admin/repositories/ld-api/api/www"
+tmux split-window -d -p 25 -v -t 1 -c "/home/debian-admin/repositories/ld-api/api/www"
+tmux new-window -t "crana:2" -n crana -c "/home/debian-admin/repositories/crana/frontend/www"
+tmux new-window -t "crana:3" -n angular-backend -c "/home/debian-admin/repositories/angular-admin/backend/www"
+tmux new-window -t "crana:4" -n angular-frontend -c "/home/debian-admin/repositories/angular-admin/frontend/www"
+tmux new-window -t "crana:0" -n bash -c $HOME
 
-tmux new-window -t "code:2" -n PHPUnit    -c "/home/crana-admin/workspace/crana/backend/www/tests"
-tmux new-window -t "code:3" -n cranafront    -c "/home/crana-admin/workspace/crana/frontend/www"
-tmux new-window -t "code:0" -n bash -c "$HOME"
-
-# 2nd session for server and apidocs
+# 2nd session (ocelot/varien/gov-quote)
 cd $HOME
-tmux new-session -s "server" -n docker -d -c "/home/crana-admin/workspace/crana/docker"
-tmux splitw -v -p 30 -t 0
-tmux new-window -t "server:2" -n apidocs -c "/home/crana-admin/workspace/crana/apidocs"
-tmux splitw -v -p 30 -t 0
+tmux new-session -s "aa" -n ocelot -d -c "/home/debian-admin/repositories/ocelot-main"
+tmux new-window -t "aa:2" -n varien -c "/home/debian-admin/repositories/4ink-main"
+tmux new-window -t "aa:3" -n gov-quote -c "/home/debian-admin/repositories/gov-quote"
 
-# link bash window between session
-# tmux kill-window -t "code:1"			# kill off code's bash
-tmux link-window -s code:0 -t server:0	# share bash
+# 3rd session (qst)
+cd $HOME
+tmux new-session -s "qst" -n qst -d -c "/home/debian-admin/repositories/qst/quikshiptoner.com/www"
+
+# 4th session (docker)
+tmux new-session -s "docker" -n docker -d -c "/home/debian-admin/repositories/ld-api/docker"
+tmux split-window -d -p 50 -c "/home/debian-admin/repositories/4ink-main/docker"
+tmux split-window -d -p 50 -t 0 -c "/home/debian-admin/repositories/crana/docker"
+tmux split-window -d -p 50 -t 2 -c "/home/debian-admin/repositories/qst/quikshiptoner.com/www"
+
+# 5th session (for debugging live magento site)
+tmux new-session -s "live-magento" -n frontend-code -d
+tmux split-window -d -p 50 \; split-window -d -p 50 -t 0 \; split-window -d -p 50 -t 2
+tmux new-window -t "live-magento:2" -n frontend-log
+tmux split-window -d -p 50 \; split-window -d -p 50 -t 0 \; split-window -d -p 50 -t 2
+tmux new-window -t "live-magento:3" -n backend-code \; split-window -d -p 50
+tmux new-window -t "live-magento:4" -n backend-log \; split-window -d -p 50
+
+# options
+# [-dhvP] [-c start-directory] [-F format] [-p percentage|-l size] [-t target-pane] [command] 
+
+# share bash
+tmux link-window -s crana:0 -t aa:0
+tmux link-window -s crana:0 -t qst:0
+tmux link-window -s crana:0 -t docker:0
+tmux link-window -s crana:0 -t live-magento:0
 
 # select window and attach session
-tmux select-window -t "code:1"
-tmux -2 attach-session -t "code"
+tmux select-window -t "crana:1"
+tmux -2 attach-session -t "crana"
 
